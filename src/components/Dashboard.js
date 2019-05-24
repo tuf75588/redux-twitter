cdf59c4aba8c31d8de1c9118a9cc68a8ed5fe901
Andrew Davis <atd285@gmail.com>
2019-05-24 14:32:30 -0400

    Update Dashboard.js

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,34 +1,29 @@
-import React, { Component } from 'react';
-import { connect } from 'react-redux';
-import Tweet from './Tweet';
-//!connect our app to redux
+import React, { Component } from 'react'
+import { connect } from 'react-redux'
+import Tweet from './Tweet'
+
 class Dashboard extends Component {
   render() {
-    const { id } = this.props;
-
     return (
-      <>
-        <div className='dashboard'>
-          <h2 className='timeline'>Your Timeline</h2>
-          <div>
-            <ul className='dashboard-list'>
-              {id.map((id) => (
-                <li key={id}>
-                  <Tweet id={id} />
-                </li>
-              ))}
-            </ul>
-          </div>
-        </div>
-      </>
-    );
+      <div>
+        <h3 className='center'>Your Timeline</h3>
+        <ul>
+          {this.props.tweetIds.map((id) => (
+            <li key={id}>
+              <Tweet id={id}/>
+            </li>
+          ))}
+        </ul>
+      </div>
+    )
   }
 }
-function mapStateToProps({ tweets, authedUser, users }) {
-  //! we need each tweet id?
-  const id = Object.keys(tweets).sort((a, b) => tweets[b].timestamp - tweets[a].timestamp);
+
+function mapStateToProps ({ tweets }) {
   return {
-    id
-  };
+    tweetIds: Object.keys(tweets)
+      .sort((a,b) => tweets[b].timestamp - tweets[a].timestamp)
+  }
 }
-export default connect(mapStateToProps)(Dashboard);
+
+export default connect(mapStateToProps)(Dashboard)
