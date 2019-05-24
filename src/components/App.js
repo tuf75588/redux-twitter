83d12682ac7a560090641173c4db0c016abffa69
Andrew Davis <atd285@gmail.com>
2019-05-24 14:32:10 -0400

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,42 +1,42 @@
-import React, { Component } from 'react';
-import { connect } from 'react-redux';
-import { handleInitialData } from '../actions/shared';
-import Dashboard from './Dashboard';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
-import LoadingBar from 'react-redux-loading';
-import New from './New';
-import TweetPage from './TweetPage';
-import Nav from './Nav';
+import React, { Component, Fragment } from 'react'
+import { BrowserRouter as Router, Route } from 'react-router-dom'
+import { connect } from 'react-redux'
+import { handleInitialData } from '../actions/shared'
+import Dashboard from './Dashboard'
+import LoadingBar from 'react-redux-loading'
+import NewTweet from './NewTweet'
+import TweetPage from './TweetPage'
+import Nav from './Nav'
+
 class App extends Component {
   componentDidMount() {
-    const { dispatch } = this.props;
-    dispatch(handleInitialData());
+    this.props.dispatch(handleInitialData())
   }
   render() {
     return (
       <Router>
-        <>
+        <Fragment>
           <LoadingBar />
           <div className='container'>
             <Nav />
-            {this.props.loading === true ? null : (
-              <div>
-                <Route path='/' exact component={Dashboard} />
-                <Route path='/tweet/:id' component={TweetPage} />
-                <Route path='/new' component={New} />
-              </div>
-            )}
+            {this.props.loading === true
+              ? null
+              : <div>
+                  <Route path='/' exact component={Dashboard} />
+                  <Route path='/tweet/:id' component={TweetPage} />
+                  <Route path='/new' component={NewTweet} />
+                </div>}
           </div>
-        </>
+        </Fragment>
       </Router>
-    );
+    )
   }
 }
-function mapStateToProps({ authedUser }) {
+
+function mapStateToProps ({ authedUser }) {
   return {
     loading: authedUser === null
-  };
+  }
 }
-const mapDispatchToProps = {};
 
-export default connect(mapStateToProps)(App);
+export default connect(mapStateToProps)(App)
