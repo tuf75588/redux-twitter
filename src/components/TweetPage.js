6c59809518f5a9cadf10f2fc01c8132322252317
Andrew Davis <atd285@gmail.com>
2019-05-24 14:42:11 -0400

    Update TweetPage.js

--- src/components/TweetPage.js
+++ src/components/TweetPage.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import Tweet from './Tweet';
-import NewTweet from './New';
+import NewTweet from './NewTweet';
 
 class TweetPage extends Component {
   render() {
